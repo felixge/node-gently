92da51315c359122b64c37f9d1ce2d4f1ca26914
PlNG <tempeml@hotmail.com>
2011-10-25 02:43:33 -0300

    Satisfies JSLint with settings /*jslint node: true, sloppy: true, vars: true, white: true, nomen: true, plusplus: true*/ /*GLOBAL assert*/

--- example/dog.js
+++ example/dog.js
@@ -1,20 +1,20 @@
 require('../test/common');
 function Dog() {}
 
-Dog.prototype.seeCat = function() {
+Dog.prototype.seeCat = function () {
   this.bark('whuf, whuf');
   this.run();
-}
+};
 
-Dog.prototype.bark = function(bark) {
+Dog.prototype.bark = function (bark) {
   require('sys').puts(bark);
-}
+};
 
-var gently = new (require('gently'))
+var gently = new(require('gently'))()
   , assert = require('assert')
   , dog = new Dog();
 
-gently.expect(dog, 'bark', function(bark) {
+gently.expect(dog, 'bark', function (bark) {
   assert.equal(bark, 'whuf, whuf');
 });
 gently.expect(dog, 'run');
