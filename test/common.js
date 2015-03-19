9e56eea1f26bb07e2fcd2eae8f585c77418833de
Kevin van Zonneveld <kevin@vanzonneveld.net>
2015-03-19 15:45:33 +0100

    Deprecate sys module in favor of util & console

--- test/common.js
+++ test/common.js
@@ -1,6 +1,8 @@
 var path = require('path')
-  , sys = require('sys');
+  , util = require('util');
 
-global.puts = sys.puts;
-global.p = function() {sys.error(sys.inspect.apply(null, arguments))};;
+global.puts = console.log;
+global.p = function() {
+  console.error(util.inspect.apply(null, arguments));
+};
 global.assert = require('assert');
